b769cee4086e00c5651601cc6a6623fe7d8e68e6
Duncan Hall <himself@duncanhall.net>
2016-01-11 15:59:35 +0000

    Return exif data after file upload :)

--- content/plugins/image_processors/postprocess.js
+++ content/plugins/image_processors/postprocess.js
@@ -1,15 +1,22 @@
 var Promise = require('bluebird');
+var ExifImage = require('exif').ExifImage;
 
 function postProcessImage (image) {
     return new Promise(function (resolve, reject) {
-        var data = image;
-        if (typeof image === 'string') {
-            data = {
-                url: image
-            }
+        try {
+            new ExifImage({image: image.original}, function (error, exifData) {
+                if (error) {
+                    reject(error.message);
+                } else {
+                    resolve({
+                        url: image.url,
+                        meta: exifData
+                    });
+                }
+            });
+        } catch (error) {
+            reject(error.message);
         }
-        data.meta = 'STUFF';
-        resolve(data);
     });
 }
 
--- core/server/api/upload.js
+++ core/server/api/upload.js
@@ -10,7 +10,10 @@ var config  = require('../config'),
 function imageSaver (store) {
     return function (uploadimage) {
         return store.save(uploadimage).then(function (url) {
-            return url;
+            return {
+                url: url,
+                original: uploadimage.path
+            };
         });
     }
 }
