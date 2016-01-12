624951181592b43c642f38d6748350086af576c9
Duncan Hall <himself@duncanhall.net>
2016-01-12 11:28:24 +0000

    Add geo and date formatting

--- core/client/app/utils/exif-formatter.js
+++ core/client/app/utils/exif-formatter.js
@@ -1,26 +1,34 @@
 
+
+function formatDegreesMinutesSeconds (components, direction) {
+    return `${components[0]}° ${components[1]}′ ${components[2]}″ ${direction}`;
+}
+
+function formatTimestamp (time) {
+    return time.split(' ').reduce((d, t) => `${d.replace(/\:/g, '-')} ${t}`);
+}
+
 export default function formatExifForOutput (exif) {
+
+    let latitude = formatDegreesMinutesSeconds(exif.gps.GPSLatitude, exif.gps.GPSLatitudeRef);
+    let longitude = formatDegreesMinutesSeconds(exif.gps.GPSLongitude, exif.gps.GPSLongitudeRef);
+
     let output = `\n<table>
     <tr>
-        <td>Focal Length: ${exif.exif.FocalLength}</td>
-        <td>Camera: ${exif.image.Make} ${exif.image.Model}</td>
+        <td>Focal Length:</td><td>${exif.exif.FocalLength}</td>
+        <td>Camera:</td><td>${exif.image.Make} ${exif.image.Model}</td>
     </tr>
     <tr>
-        <td>ISO: ${exif.exif.ISO}</td>
-        <td>Date: ${exif.exif.DateTimeOriginal}</td>
+        <td>ISO:</td><td>${exif.exif.ISO}</td>
+        <td>Date:</td><td>${exif.exif.DateTimeOriginal}</td>
     </tr>
     <tr>
-        <td>F Number: ${exif.exif.FNumber}</td>
-        <td>Latitude: ${exif.gps.GPSLatitude}</td>
+        <td>F Number:</td><td>${exif.exif.FNumber}</td>
+        <td>Latitude:</td><td>${latitude}</td>
     </tr>
     <tr>
-        <td>Shutter Speed: ${exif.exif.ShutterSpeedValue}</td>
-        <td>Longitude: ${exif.gps.GPSLongitude}</td>
-
-    </tr>
-    <tr>
-        <td>Exposure: ${exif.exif.ExposureTime}</td>
-        <td></td>
+        <td>Shutter Speed:</td><td>${exif.exif.ShutterSpeedValue}</td>
+        <td>Longitude:</td><td>${longitude}</td>
     </tr>
 </table>`;
 
